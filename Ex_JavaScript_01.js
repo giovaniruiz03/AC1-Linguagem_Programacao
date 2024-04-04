e92da89e3fe9442b08ab73aab36b4936316c12e3
giovaniruiz03 <145368122+giovaniruiz03@users.noreply.github.com>
2024-04-04 20:33:54 -0300

    Ex_JavaScript_01.js

---
+++ Ex_JavaScript_01.js
@@ -0,0 +1,14 @@
+var numero1 = 20;
+var numero2 = 4;
+
+
+var soma = numero1 + numero2;
+var subtracao = numero1 - numero2;
+var multiplicacao = numero1 * numero2;
+var divisao = numero1 / numero2;
+
+
+console.log("Soma:", numero1, '+' ,numero2, '=' ,soma);
+console.log("Subtração:", numero1, '-' ,numero2, '=' ,subtracao);
+console.log("Multiplicação:", numero1, '*' ,numero2, '=' ,multiplicacao);
+console.log("Divisão:", numero1, '/' ,numero2, '=' ,divisao);
