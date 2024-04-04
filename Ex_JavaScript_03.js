421ccf93a5ff703fcba08b47c6406957ebc0ad46
giovaniruiz03 <145368122+giovaniruiz03@users.noreply.github.com>
2024-04-04 20:42:34 -0300

    Ex_JavaScript_03.js

---
+++ Ex_JavaScript_03.js
@@ -0,0 +1,4 @@
+var texto = "Disciplina de Programação para web";
+var qtdCaracteres = texto.length;
+
+console.log("A quantidade de caracteres no texto 'Disciplina de Programação para web' são:", qtdCaracteres, 'letras');
